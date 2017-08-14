8c6db6da5f2a37e7b8d55a61426d525e434bbfd1
Kate Sohng <kheey@yg-laptop.redmond.corp.microsoft.com>
2017-08-13 20:13:01 -0700

    webpack asset and development

--- webpack2/src/index.js
+++ webpack2/src/index.js
@@ -1,11 +1,21 @@
 import _ from 'lodash';
+import './style.css';
+import CakeImage from './bacon-pancake.jpg';
+import printMe from './print.js';
 
-function compinent() {
+function component() {
     var element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    var cakePic = new Image();
+    cakePic.src = CakeImage;
+
+    element.appendChild(cakePic);
 
     return element;
 }
 
-document.body.appendChild(compinent());
+document.body.appendChild(component());
+printMe();
