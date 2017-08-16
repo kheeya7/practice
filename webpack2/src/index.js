590725a8b6a344bad880120a5d7735161a6f516d
Kate Sohng <kheey@yg-laptop.redmond.corp.microsoft.com>
2017-08-15 20:38:05 -0700

    pug initiation

--- webpack2/src/index.js
+++ webpack2/src/index.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import './style.css';
 import CakeImage from './bacon-pancake.jpg';
 import printMe from './print.js';
+import template from './first.pug';
 
 function component() {
     var element = document.createElement('div');
@@ -9,10 +10,37 @@ function component() {
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
 
-    var cakePic = new Image();
-    cakePic.src = CakeImage;
+    // var cakePic = new Image();
+    // cakePic.src = CakeImage;
 
-    element.appendChild(cakePic);
+    // element.appendChild(cakePic);
+
+    var element2 = document.createElement('div');
+
+    var itemData = [
+    {
+        title: 'This is the title, yo',
+        description: 'This is the spot for description.',
+        price: 100,
+        buttonDescription: 'Pick me'
+    },
+    {
+        title: 'This is another title, yo',
+        description: 'This is the spot for another description.',
+        price: 800,
+        buttonDescription: 'choose me'
+    },
+    {
+        title: 'This is the last title, yo',
+        description: 'This is the spot for the last description.',
+        price: 1000,
+        buttonDescription: 'Pick me up'
+    }    
+];
+
+    element2.innerHTML = template({items: itemData});
+
+    element.appendChild(element2);
 
     return element;
 }
