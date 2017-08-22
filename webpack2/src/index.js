a1391d308acbfd907d9b29c465de7fa19d220527
Kate Sohng <kheey@yg-laptop.redmond.corp.microsoft.com>
2017-08-21 22:41:19 -0700

    saving the pug progress

--- webpack2/src/index.js
+++ webpack2/src/index.js
@@ -2,45 +2,22 @@ import _ from 'lodash';
 import './style.css';
 import CakeImage from './bacon-pancake.jpg';
 import printMe from './print.js';
-import template from './first.pug';
+import '../node_modules/bootstrap';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import CoolItems from './cool-items/cool-items.js';
+
 
 function component() {
     var element = document.createElement('div');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
+    var coolItemsPanel = document.createElement('div');
 
-    // var cakePic = new Image();
-    // cakePic.src = CakeImage;
+    var coolItems = new CoolItems();
+    var coolItemsHtml = coolItems.render();
 
-    // element.appendChild(cakePic);
+    coolItemsPanel.innerHTML = coolItemsHtml;
 
-    var element2 = document.createElement('div');
-
-    var itemData = [
-    {
-        title: 'This is the title, yo',
-        description: 'This is the spot for description.',
-        price: 100,
-        buttonDescription: 'Pick me'
-    },
-    {
-        title: 'This is another title, yo',
-        description: 'This is the spot for another description.',
-        price: 800,
-        buttonDescription: 'choose me'
-    },
-    {
-        title: 'This is the last title, yo',
-        description: 'This is the spot for the last description.',
-        price: 1000,
-        buttonDescription: 'Pick me up'
-    }    
-];
-
-    element2.innerHTML = template({items: itemData});
-
-    element.appendChild(element2);
+    element.appendChild(coolItemsPanel);
 
     return element;
 }
