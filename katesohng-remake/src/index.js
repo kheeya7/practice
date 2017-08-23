c8db5fd882d2b7e13917f895442627031e5e66af
Kate Sohng <kheey@yg-laptop.redmond.corp.microsoft.com>
2017-08-22 23:16:28 -0700

    Modify the intro section

--- katesohng-remake/src/index.js
+++ katesohng-remake/src/index.js
@@ -1,26 +1,9 @@
-import _ from 'lodash';
 import './style.css';
-import CakeImage from './bacon-pancake.jpg';
-import printMe from './print.js';
 import '../node_modules/bootstrap';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import CoolItems from './cool-items/cool-items.js';
+import RootPage from './rootpage/rootpage.js';
 
+var rootpage = new RootPage();
+var rootpageHtml = rootpage.render();
 
-function component() {
-    var element = document.createElement('div');
-
-    var coolItemsPanel = document.createElement('div');
-
-    var coolItems = new CoolItems();
-    var coolItemsHtml = coolItems.render();
-
-    coolItemsPanel.innerHTML = coolItemsHtml;
-
-    element.appendChild(coolItemsPanel);
-
-    return element;
-}
-
-document.body.appendChild(component());
-printMe();
+$('body').append(rootpageHtml);
