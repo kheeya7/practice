22108b79fc42769301c116a9322f35655828cc66
Kate Sohng <kheeya7@hotmail.com>
2018-12-13 17:01:07 -0800

    Update img markup with srcset and sizes attributes

--- auto-responsive-img-markup/JS/script.js
+++ auto-responsive-img-markup/JS/script.js
@@ -1,4 +1,10 @@
 const IMAGES = document.querySelectorAll("img");
+const SIZES =  {
+    showcase: "100vw",
+    reason: "(max-width: 799px) 100vw, 372px",
+    feature: "(max-width: 799px) 100vw, 558px",
+    story: "(max-width: 799px) 100vw, 670px",
+};
 
 function makeSrcset(imgSrc) {
     let markup = [];
@@ -16,8 +22,9 @@ for (let i = 0; i<IMAGES.length; i++) {
     let imgSrc = IMAGES[i].getAttribute("src") ;
     imgSrc = imgSrc.slice(0, -8);
     let srcset = makeSrcset(imgSrc);
-    console.log(srcset);
+    IMAGES[i].setAttribute("srcset", srcset);
 
     let type = IMAGES[i].getAttribute("data-type");
-    console.log(type);
+    let sizes = SIZES[type];
+    IMAGES[i].setAttribute("sizes", sizes);
 }
